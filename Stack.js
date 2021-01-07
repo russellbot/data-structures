f2d2f33b1e4fc6099d80ea21e7a72b75627515e7
Russell Hart <russellbot@gmail.com>
2021-01-07 11:38:55 -0500

    added methods

--- Stack.js
+++ Stack.js
@@ -1,24 +1,57 @@
 class Node {
     constructor(value){
-      this.value = value;
-      this.next = null;
+        this.value = value;
+        this.next = null;
     }
-  }
+}
   
-  class Stack {
+class Stack {
     constructor(){
-      this.top = null;
-      this.bottom = null;
-      this.length = 0;
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
     }
+  
     peek() {
+        return this.top;
     }
+  
     push(value){
+        let newNode = new Node(value);
+        if (!this.top) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else if (this.length === 1) {
+            this.bottom = this.top;
+            this.top = newNode;
+        } else {
+            newNode.next = this.top
+        }
+        this.length++;
     }
+  
     pop(){
+        if (this.isEmpty()) {
+            return 'Stack is Empty';
+        } else {
+            this.top = this.top.next;
+        }
     }
-    //isEmpty
-  }
   
-  const myStack = new Stack();
+    isEmpty() {
+        if (this.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
   
+const myStack = new Stack();
+  
+myStack.peek();
+
+myStack.push('Google');
+myStack.push('Udemy');
+myStack.push('Discord');
+
