f4f27bdb02fbe266ef3b5846239108fd101d7e13
Russell Hart <russellbot@gmail.com>
2021-01-13 13:01:00 -0500

    shortened recursive code

--- Algorithms/Recursion/Factorial.js
+++ Algorithms/Recursion/Factorial.js
@@ -1,19 +1,17 @@
 // Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
-let answer = 1;
+
 function findFactorialRecursive(number) {
-  let counter = number;
-  
-  if (counter < 2) {
-    answer = answer*counter;
-    return answer;
-  }
-  answer = answer*counter;
-  counter--;
-  return findFactorialRecursive(counter);
+    if (number === 2) {
+      return 2;
+    }
+    return number * findFactorialRecursive(number-1);
 }
 
 function findFactorialIterative(number) {
   let answer = 1;
+  if (number === 2) {
+      answer = 2;
+  }
   for (let i = number; i > 1; i--) {
     answer = i * answer;
   }
