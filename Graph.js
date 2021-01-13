d4a7bd1e2fb3aba21f39ed0bd8f210c3a2b4a049
Russell Hart <russellbot@gmail.com>
2021-01-13 09:47:59 -0500

    addVertex and addEdge methods

--- Graph.js
+++ Graph.js
@@ -1,24 +1,30 @@
 class Graph { 
     constructor() { 
-      this.numberOfNodes = 0;
-      this.adjacentList = {
-      }; 
+        this.numberOfNodes = 0;
+        this.adjacentList = {
+        }; 
     } 
     addVertex(node)  { 
+        this.adjacentList[node] = [];
+        this.numberOfNodes++;
+        return this;
     } 
     addEdge(node1, node2) { 
-      //undirected Graph 
-    } 
+        //undirected Graph
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);  
+        return this;
+    }
     showConnections() { 
-      const allNodes = Object.keys(this.adjacentList); 
-      for (let node of allNodes) { 
-        let nodeConnections = this.adjacentList[node]; 
-        let connections = ""; 
-        let vertex;
-        for (vertex of nodeConnections) {
-          connections += vertex + " ";
+        const allNodes = Object.keys(this.adjacentList); 
+        for (let node of allNodes) { 
+            let nodeConnections = this.adjacentList[node]; 
+            let connections = ""; 
+            let vertex;
+            for (vertex of nodeConnections) {
+            connections += vertex + " ";
+            } 
+            console.log(node + "-->" + connections); 
         } 
-        console.log(node + "-->" + connections); 
-      } 
-  } 
+    } 
 } 
