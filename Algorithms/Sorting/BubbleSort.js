6b3e8b99f4905814203682b1d8919dcb8c8a90e8
Russell Hart <russellbot@gmail.com>
2021-01-14 11:13:23 -0500

    cleanup

--- Algorithms/Sorting/BubbleSort.js
+++ Algorithms/Sorting/BubbleSort.js
@@ -1,8 +1,8 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
-  for (i=0; i<array.length-1; i++) {
-    for(j=0; j<array.length-1-i; j++) {
+  for (let i=0; i<array.length-1; i++) {
+    for (let j=0; j<array.length-1-i; j++) {
       let value1 = array[j];
       let value2 = array[j+1];
       if(value1 > value2) {
